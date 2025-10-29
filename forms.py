f894234d3b3f1dc2ac8a92f06834f408c6f4421e
zfiha <zannatulfiha8@gmail.com>
2025-10-29 08:37:56 +0600

    forms.py

---
+++ forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Feedback
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Feedback'}),
+        }
